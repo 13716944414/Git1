722c87eb213975547d49adcc549fced4cc5871ab
jiangjiaxin <ask3985@163.com>
2021-06-09 18:05:40 +0800

    首次提交

---
+++ src/main/java/com/ask/HelloGit.java
@@ -0,0 +1,13 @@
+package com.ask;
+
+/**
+ * @author jiangjiaxin
+ * @version 0.1.0
+ * @create 2021-06-09 17:55
+ * @since 0.1.0
+ **/
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("local create file ");
+    }
+}
---
+++ src/main/java/com/ask/HelloGit1.java
@@ -0,0 +1,10 @@
+package com.ask;
+
+/**
+ * @author jiangjiaxin
+ * @version 0.1.0
+ * @create 2021-06-09 18:03
+ * @since 0.1.0
+ **/
+public class HelloGit1 {
+}
